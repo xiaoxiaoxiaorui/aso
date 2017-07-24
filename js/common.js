21c2dfd88e3f82cd1eecb52a79f8bac2e0437a69
xiaoxiaoxiaorui <7743600989@qq.com>
2017-07-24 18:47:10 +0800

    首页基本完成，还需更换文字，更改细节

--- js/common.js
+++ js/common.js
@@ -5,7 +5,7 @@
 
 $(function () {
 
-    //    nav
+            //    nav
 
     var w = window.innerWidth;
     if( w < 768){
@@ -39,4 +39,28 @@ $(function () {
             });
         }
     });
+
+
+
+            //    index country select
+    function formatState (state) {
+        if (!state.id) { return state.text; }
+        /*var scr = "Images/" + state.element.value + ".png";*/
+        var $state = $(
+            '<div class="flag-div"><img src="Images/' + state.element.value + '.png" class="img-flag" /> ' + '</div><span>' + state.text + '</span>'
+            /*'<span style="background:url(' + scr + ');background-size:100%;float:left;margin:5px 10px 0 0;">' + '<div class="img-flag"></div></span><span>' + state.text + '</span>'*/
+        );
+        return $state;
+    }
+    $(".js-example-templating").select2({
+        templateResult: formatState,
+        templateSelection: formatState
+    }).change(function () {
+        $(".select2-selection__rendered span:last-child").html("");
+    });
+    $(".select2").width('100%');
+    $(".select2-selection__rendered span:last-child").html("");
+
+
+    $(".select2").append("<input type='text' class='search-word' placeholder='Type Keyword, App Name, App ID or URL'>")
 });
